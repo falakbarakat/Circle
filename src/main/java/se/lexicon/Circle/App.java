8eef174aa321d2321be4087aa948fb048357f6e2
falakbarakat <45755774+falakbarakat@users.noreply.github.com>
2019-01-31 12:03:40 +0100

    commit

---
+++ src/main/java/se/lexicon/Circle/App.java
@@ -0,0 +1,18 @@
+package se.lexicon.Circle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	// public double radius;
+	     public static void main( String[] args ){
+	    /*	 double radius = 20;
+	      Circle c1=new Circle();
+	      System.out.println("area"+c1.area(radius));*/
+	    	 Circle c1=new Circle();
+	    	 System.out.println(c1.area(10.0));
+	    }
+
+}
