091474373bee22418dff1f007a50123c8fbb87e1
falakbarakat <45755774+falakbarakat@users.noreply.github.com>
2019-01-31 12:04:03 +0100

    commit

---
+++ src/main/java/se/lexicon/Circle/Circle.java
@@ -0,0 +1,38 @@
+	package se.lexicon.Circle;
+
+public class Circle {
+	
+	 private double radius;
+	 private double area;
+	
+	 
+	/* public Circle (){
+		   this.radius=radius;
+		   this.area=area;
+	  }*/
+	
+     public double area(double radius) {
+    	 return area=3.14*radius*radius;
+        	 
+     }
+}
+ 
+ 
+ 
+/*public double getRadius() {
+	return radius;
+}
+public void setRadius(double radius) {
+	this.radius = radius;
+}
+public double getArea(double radius) {
+	return area=3.14*radius*radius;
+}
+public void setArea(double area) {
+	this.area = area;
+}
+ 
+}	
+
+	  
+*/
